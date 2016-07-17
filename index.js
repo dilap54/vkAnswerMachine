f7b1a21b4d42955b400b9771c2e7ea3332f53a10
dilap54 <dilap54@mail.ru>
2016-07-18 00:29:33 +0300

    remove url require

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // https://oauth.vk.com/authorize?client_id=5550899&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=4098&response_type=token&v=5.52&state=4itProductions
 // https://{$server}?act=a_check&key={$key}&ts={$ts}&wait=25&mode=2 
 var https = require('https');
-var url = require('url');
 var querystring = require('querystring');
 var config = require('./config.js');
 
