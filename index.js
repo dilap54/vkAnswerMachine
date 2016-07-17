4078811d4a7f86d17dcfe17c63d91657ac2f6548
dilap54 <dilap54@mail.ru>
2016-07-17 21:41:24 +0300

    filter work.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 // https://{$server}?act=a_check&key={$key}&ts={$ts}&wait=25&mode=2 
 var https = require('https');
 var config = require('./config.js');
+
+
 function now(){
 	var now = new Date();
     return	now.getDate()+"."+
@@ -12,6 +14,37 @@ function now(){
 			now.getSeconds()+"."+
 			now.getMilliseconds();
 }
+if (![].includes) {//Полифилл Array.prototype.includes()
+	Array.prototype.includes = function(searchElement/*, fromIndex*/) {
+		'use strict';
+		var O = Object(this);
+		var len = parseInt(O.length) || 0;
+		if (len === 0) {
+			return false;
+		}
+		var n = parseInt(arguments[1]) || 0;
+		var k;
+		if (n >= 0) {
+			k = n;
+		} else {
+			k = len + n;
+			if (k < 0) {
+				k = 0;
+			}
+		}
+		while (k < len) {
+			var currentElement = O[k];
+			if (searchElement === currentElement ||
+				(searchElement !== searchElement && currentElement !== currentElement)
+			) {
+				return true;
+			}
+			k++;
+		}
+		return false;
+	};
+}
+
 var AnswerMachine = function(token){
 	this.token = token;
 	this.longPollServer = {};
@@ -75,13 +108,14 @@ var AnswerMachine = function(token){
 		})
 		.on('timeout', function(){
 			console.error(now(), 'longPollReq timeout');
+			that.lastLongPollReq.end();
 			that.sendLongPoll();
 		})
 		.setTimeout(30000);
 	}
 	this.getLongPollServer();
 
-	//fail restart
+	
 	var that = this;
 	function checkHealth(){
 		if (Date.now() - that.lastLongPollQueryDate.getTime() > 1000*60*2){
@@ -90,7 +124,36 @@ var AnswerMachine = function(token){
 			that.getLongPollServer();
 		}
 	}
-	this.failRestartTimer = setInterval(checkHealth, 1000*60);
+	this.failRestartTimer = setInterval(checkHealth, 1000*60);//Чинит скрипт, если все плохо.
+
+
+	this.friends = [];
+	this.getFriends = function(){
+		console.log(now(), 'update friends');
+		var that = this;
+		https.get('https://api.vk.com/method/friends.get?&access_token='+this.token+'&v=5.52', function(res){
+			var body = '';
+			res.on('data', function(chunk){
+				body+=chunk;
+			});
+			res.on('end', function(){
+				body = JSON.parse(body);
+				if (body.response){
+					console.log(now(), 'Friends loaded', body.response.count);
+					that.friends = body.response.items;
+				}
+				else{
+					console.error('body', body);
+				}
+			});	
+		})
+		.on('error', function(e) {
+			console.error(e);
+		});
+	}
+	this.getFriends();
+	this.updateFriendsUpdateTimer = setInterval(this.getFriends, 1000*60*60);
+
 
 	this.processMessage = function(message){
 		var msg = {
@@ -102,9 +165,14 @@ var AnswerMachine = function(token){
 			text: message[6]
 		}
 		console.log(msg);
+		if (!that.friends.includes(msg.from_id) && msg.from_id<2000000000){
+			console.log('process');
+		}
 	}
-
+	
 
 }
+function Answerer(){
 
+}
 var answerMachine = new AnswerMachine(config.token);
